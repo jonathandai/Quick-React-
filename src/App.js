6f5922214653ad594944afb979935eae3e6b477e
Jonathan Dai <jonathandai1226@gmail.com>
2019-09-27 13:37:22 -0500

    third slice

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import 'rbx/index.css';
+import { Button, Container, Title } from 'rbx';
 
 const Banner = ({ title }) => (
-  <h1>{ title }</h1>
+  <Title>{ title }</Title>
 );
 
 const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
@@ -15,15 +17,15 @@ const getCourseNumber = course => (
 )
   
 const Course = ({ course }) => (
-  <button>
+  <Button>
     { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
-  </button>
+  </Button>
 );
 
 const CourseList = ({ courses }) => (
-  <div>
-    { courses.map(course => <Course key={course.id} course={ course } />) }
-  </div>
+  <Button.Group>
+    {courses.map(course => <Course key={course.id} course={ course } />)}
+  </Button.Group>
 );
 
 const schedule = {
@@ -53,10 +55,10 @@ const schedule = {
 };
 
 const App = () =>  (
-  <div>
+  <Container>
     <Banner title={ schedule.title } />
     <CourseList courses={ schedule.courses } />
-  </div>
+  </Container>
 );
 
 export default App;
